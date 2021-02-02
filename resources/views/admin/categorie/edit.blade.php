ffbbecf5226c826446cc8ba55a17095a6a35d27b
ronaldokana <ronaldokana12@gmail.com>
2021-02-02 01:49:03 +0100

    update category for admin

--- resources/views/admin/categorie/edit.blade.php
+++ resources/views/admin/categorie/edit.blade.php
@@ -1,6 +1,6 @@
 @extends('admin.layouts.master')
 
-@section('page-header', 'Edit Super Category')
+@section('page-header', 'Edit  Category')
 
 
 
@@ -9,15 +9,15 @@
  <div  class="card card-small mb-4 mt-4">
     <div class="row">
         <div class="col-sm-12 col-md-12 pl-5 pr-5">
-            <strong class="text-muted d-block mb-2">Edit Super Category</strong>
+            <strong class="text-muted d-block mb-2">Edit  Category</strong>
             
-            <form action="{{ route('admin.category.update',$rep->id) }}" method="POST">
+            <form action="{{ route('admin.categorie.update',$recup->id) }}" method="POST">
                 @csrf   
                 @method('PUT')
                 
                 <div class="form-group">
                     <label for="name">name</label>
-                    <input type="text" class="form-control" placeholder="Name" id="name" name="name" value="{{$rep->name}}"> 
+                    <input type="text" class="form-control" placeholder="Name" id="name" name="name" value="{{$recup->name}}"> 
                     @error('name')
                         <div class="invalid-feedback">{{ $message }}</div>
                     @enderror  
@@ -33,7 +33,9 @@
 
                 <div class="form-group">
                     <label for="description">Description</label>
-                    <textarea type="text" class="form-control" id="description" name="description"  value="{{$rep->description}}"></textarea>
+                    <textarea type="text" class="form-control" id="description" name="description"  value="{{$recup->description}}">
+                      {{ old('description', $recup->description) }}
+                    </textarea>
                     @error('description')
                         <div class="invalid-feedback">{{ $message }}</div>
                     @enderror  
@@ -45,7 +47,7 @@
                             Choice a super category
                         </option>
                         @foreach($collection as $collec)
-                        <option value="{{$collec->id}}">
+                        <option value="{{$collec->id}}"  @if(old('super_category_id', $recup->super_category_id) == $collec->id) selected @endif>
                             {{$collec->name}}
                         </option>
                         @endforeach
